1dc2e6374fb5685f0a7b4d81db43fe60c5f1852b
Klaus Wenger <kjwenger@yahoo.com>
2017-08-11 14:33:07 +0200

    created swagger code

---
+++ go/pets.go
@@ -0,0 +1,50 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Pets struct {
+
+}
+
+func AddPet(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func DeletePet(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func FindPetsByStatus(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func FindPetsByTags(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func GetPetById(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func UpdatePet(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func UpdatePetWithForm(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
---
+++ go/routers.go
@@ -0,0 +1,187 @@
+package 
+
+import (
+	"net/http"
+	"fmt"
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+
+	return router
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/v2/",
+		Index,
+	},
+
+	Route{
+		"AddPet",
+		"POST",
+		"/v2/pets",
+		AddPet,
+	},
+
+	Route{
+		"DeletePet",
+		"DELETE",
+		"/v2/pets/{petId}",
+		DeletePet,
+	},
+
+	Route{
+		"FindPetsByStatus",
+		"GET",
+		"/v2/pet/findByStatus",
+		FindPetsByStatus,
+	},
+
+	Route{
+		"FindPetsByTags",
+		"GET",
+		"/v2/pets/findByTags",
+		FindPetsByTags,
+	},
+
+	Route{
+		"GetPetById",
+		"GET",
+		"/v2/pets/{petId}",
+		GetPetById,
+	},
+
+	Route{
+		"UpdatePet",
+		"PUT",
+		"/v2/pets",
+		UpdatePet,
+	},
+
+	Route{
+		"UpdatePetWithForm",
+		"POST",
+		"/v2/pets/{petId}",
+		UpdatePetWithForm,
+	},
+
+	Route{
+		"UploadFile",
+		"POST",
+		"/v2/pets/{petId}/uploadImage",
+		UploadFile,
+	},
+
+	Route{
+		"DeleteOrder",
+		"DELETE",
+		"/v2/stores/orders/{orderId}",
+		DeleteOrder,
+	},
+
+	Route{
+		"GetInventory",
+		"GET",
+		"/v2/stores/inventory",
+		GetInventory,
+	},
+
+	Route{
+		"GetOrderById",
+		"GET",
+		"/v2/stores/orders/{orderId}",
+		GetOrderById,
+	},
+
+	Route{
+		"PlaceOrder",
+		"POST",
+		"/v2/stores/orders",
+		PlaceOrder,
+	},
+
+	Route{
+		"CreateUser",
+		"POST",
+		"/v2/users",
+		CreateUser,
+	},
+
+	Route{
+		"CreateUsersWithArrayInput",
+		"POST",
+		"/v2/users/createWithArray",
+		CreateUsersWithArrayInput,
+	},
+
+	Route{
+		"CreateUsersWithListInput",
+		"POST",
+		"/v2/users/createWithList",
+		CreateUsersWithListInput,
+	},
+
+	Route{
+		"DeleteUser",
+		"DELETE",
+		"/v2/users/{username}",
+		DeleteUser,
+	},
+
+	Route{
+		"GetUserByName",
+		"GET",
+		"/v2/users/{username}",
+		GetUserByName,
+	},
+
+	Route{
+		"LoginUser",
+		"GET",
+		"/v2/users/login",
+		LoginUser,
+	},
+
+	Route{
+		"LogoutUser",
+		"GET",
+		"/v2/users/logout",
+		LogoutUser,
+	},
+
+	Route{
+		"UpdateUser",
+		"PUT",
+		"/v2/users/{username}",
+		UpdateUser,
+	},
+
+}
